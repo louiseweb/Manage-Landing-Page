d87d11f27e4fe8cde3cbf158df012f4ed490f3fe
Louise Webster <louise_webster@mail.com>
2022-09-09 16:51:24 +0100

    Started layout for testimonial slider and footer

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ const openBtn =  document.querySelector('#open-btn');
 const closeBtn =  document.querySelector('#close-btn');
 const nav =  document.querySelector('#nav');
 const menuOverlay = document.querySelector('#menu-overlay');
+const body = document.querySelector('#body');
 
 openBtn.addEventListener('click', function(){
     openBtn.classList.toggle('d-none');
     closeBtn.classList.toggle('d-none');
     nav.classList.toggle('d-none');
     menuOverlay.classList.toggle('d-none');
+    body.classList.toggle('no-scroll');
 });
 
 closeBtn.addEventListener('click', function(){
@@ -15,4 +17,44 @@ closeBtn.addEventListener('click', function(){
     openBtn.classList.toggle('d-none');
     nav.classList.toggle('d-none');
     menuOverlay.classList.toggle('d-none');
+    body.classList.toggle('no-scroll');
 });
+
+$(document).ready(function(){
+    $('.testimonial').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 768,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+    });
+  });
